3d87a502b3222a45db9e5309af5d35afc51b1ac9
Jose Garcia <mendoza.joser51@gmail.com>
2022-10-19 20:01:24 -0700

    added loading page

--- sporting-app-client/src/App.js
+++ sporting-app-client/src/App.js
@@ -1,11 +1,56 @@
+import { useState } from "react";
+
 import LoginPage from "./Components/Login/login-page/LoginPage";
+import CreateUser from "./Components/CreateUser/CreateUser";
 
 function App() {
-  return (
-    <div>
-      <LoginPage />
-    </div>
-  );
+  const [screen, setScreen] = useState("welcome");
+
+  const WelcomePage = () => {
+    return (
+      <div>
+        <div>
+          <h1>Welcome</h1>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setScreen("login");
+            }}
+          >
+            Login
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setScreen("createUser");
+            }}
+          >
+            Create User
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  const pageHandler = () => {
+    if (screen === "login") {
+      return (
+        <div>
+          <LoginPage />
+        </div>
+      );
+    } else if (screen === "createUser") {
+      return (
+        <div>
+          <CreateUser />
+        </div>
+      );
+    }
+  };
+
+  return <div>{screen === "welcome" ? WelcomePage() : pageHandler()}</div>;
 }
 
 export default App;
